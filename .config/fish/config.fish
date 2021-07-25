2c7e25b2bde55d986d4f460da3d2c5f3d4f5cdd0
Ederson Duarte Itabaiana <9091231-edertech@users.noreply.gitlab.com>
2021-07-24 23:40:47 -0300

    fish updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,13 @@ set CHROME_EXECUTABLE /usr/bin/chromium
 
 set PATH $PATH /home/ederson/.local/bin ~/.ghcup/bin $GROOVY_HOME/bin $ANDROID_HOME/platforms $ANDROID_HOME/platform-tools ~/.npm-global/bin
 
-alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/    --work-tree=$HOME'
 
 alias vim=nvim
 alias upall='yay -Syu --noconfirm'
 alias pacman='sudo pacman --color auto'
 
 set EDITOR vim
+
 
 neofetch
