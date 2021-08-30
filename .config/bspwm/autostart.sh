722b03955ee5d2d89f8ddc44f6cddbc253d81f95
Ederson Duarte Itabaiana <9091231-edertech@users.noreply.gitlab.com>
2021-08-30 07:39:13 -0300

    dual monitor config

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -10,10 +10,12 @@ function run {
 # autostart
 pgrep -x sxhkd > /dev/null || sxhkd &
 $HOME/.config/polybar/launch.sh &
+run /usr/lib/xfce-polkit/xfce-polkit &
 run blueman-applet &
 run volumeicon &
 run nm-applet &
 run /usr/bin/emacs --daemon &
+run xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --mode 1440x900 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-2 --off
 run nitrogen --restore &
 run picom &
 
