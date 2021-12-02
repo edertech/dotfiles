4f3938819af2f1e700bd4d37a71b2ed128b98222
Ederson Duarte Itabaiana <9091231-edertech@users.noreply.gitlab.com>
2021-12-02 16:58:46 -0300

    Updated doom configs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,14 +3,6 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-;; ------ starting my config -------
-;;load a file named key-chord.el from some directory in the load-path (e.g. "~/.emacs.d")
-
-(require 'key-chord)
-(key-chord-mode 1)
-(key-chord-define-global "ii" 'evil-normal-state);
-
-;; ---------------------------------
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
@@ -27,10 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "JetBrains Mono" :size 16 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "JetBrains Mono") ; inherits `doom-font''s :size
-      doom-unicode-font (font-spec :family "JetBrains Mono" :size 16)
-      doom-big-font (font-spec :family "JetBrains Mono" :size 20))
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -62,51 +52,3 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#282c34" "#C16069" "#A2BF8A" "#ECCC87" "#80A0C2" "#B58DAE" "#86C0D1" "#eceff4"])
- '(custom-safe-themes
-   '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "2cdc13ef8c76a22daa0f46370011f54e79bae00d5736340a5ddfe656a767fddf" "632694fd8a835e85bcc8b7bb5c1df1a0164689bc6009864faed38a9142b97057" "fe94e2e42ccaa9714dd0f83a5aa1efeef819e22c5774115a9984293af609fce7" "9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" "e2acbf379aa541e07373395b977a99c878c30f20c3761aac23e9223345526bcc" "912cac216b96560654f4f15a3a4d8ba47d9c604cbc3b04801e465fb67a0234f0" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" default))
- '(fci-rule-color "#525252")
- '(jdee-db-active-breakpoint-face-colors (cons "#000000" "#80A0C2"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#000000" "#A2BF8A"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#000000" "#3f3f3f"))
- '(objed-cursor-color "#C16069")
- '(package-selected-packages
-   '(neotree company-lsp python-environment company-native-complete lsp-python-ms lsp-haskell haskell-mode))
- '(pdf-view-midnight-colors (cons "#eceff4" "#323334"))
- '(rustic-ansi-faces
-   ["#323334" "#C16069" "#A2BF8A" "#ECCC87" "#80A0C2" "#B58DAE" "#86C0D1" "#eceff4"])
- '(vc-annotate-background "#323334")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#A2BF8A")
-    (cons 40 "#bac389")
-    (cons 60 "#d3c788")
-    (cons 80 "#ECCC87")
-    (cons 100 "#e3b57e")
-    (cons 120 "#da9e75")
-    (cons 140 "#D2876D")
-    (cons 160 "#c88982")
-    (cons 180 "#be8b98")
-    (cons 200 "#B58DAE")
-    (cons 220 "#b97e97")
-    (cons 240 "#bd6f80")
-    (cons 260 "#C16069")
-    (cons 280 "#a0575e")
-    (cons 300 "#804f54")
-    (cons 320 "#5f4749")
-    (cons 340 "#525252")
-    (cons 360 "#525252")))
- '(vc-annotate-very-old-color nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
